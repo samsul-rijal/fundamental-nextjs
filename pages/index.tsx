973ad1e3a574ea6c6eb3209f93002ffd89988152
Your Name <samsulr99@gmail.com>
2022-09-02 13:47:02 +0700

    styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
-import Header from '../components/header'
+import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div >
       <Header />
+
+      <p className={styles['title-homepage']}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolorum sed similique culpa, sint nobis corporis laborum asperiores. Nihil optio dolorem temporibus, numquam officiis error atque sequi quia laborum.</p>
+
+      <Footer />
     </div>
   )
 }

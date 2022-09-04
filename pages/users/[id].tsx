d2625a9ef4c22f8b40cc95c10a4c966c969840d5
Your Name <samsulr99@gmail.com>
2022-09-04 23:59:34 +0700

    dynamic routes

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,9 +1,12 @@
+import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 
 export default function UserDetail() {
+  const router = useRouter()
+  const {id} = router.query
   return (
     <Layout pageTitle="Detail User">
-      <h2>Detail User Page</h2>
+      <h2>Detail User Page {id}</h2>
     </Layout>
   )
 }

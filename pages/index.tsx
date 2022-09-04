a166856401fba61792bdeeb29c8469e8bc51743a
Your Name <samsulr99@gmail.com>
2022-09-04 23:55:13 +0700

    image optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
       <Layout pageTitle='Home Page'>
+        <Image src="/user.jpg" width={200} height={200} alt='profile' />
         <p className={styles['title-homepage']}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolorum sed similique culpa, sint nobis corporis laborum asperiores. Nihil optio dolorem temporibus, numquam officiis error atque sequi quia laborum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <Image src="/spiderman.jpg" width={200} height={200} alt='profile' />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, atque vel dicta distinctio officiis, unde optio porro aperiam numquam incidunt facilis sequi facere possimus eos quisquam! Ex excepturi vel repellat.</p>
+        <Image src="/cat.jpg" width={200} height={200} alt='profile' />
       </Layout>  
     </>
   )

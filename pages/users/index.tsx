11b2935fd931358b9a58aeca22c9fd5b6eed8a1c
Your Name <samsulr99@gmail.com>
2022-09-09 14:04:02 +0700

    9.fetch-data-getstaticprops

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,37 @@
 import Layout from "../../components/Layout";
 
-export default function User() {
+interface UserProps {
+  dataUsers: Array<any>
+}
+
+export default function User(props: UserProps) {
+  const { dataUsers } = props;
+
+  console.log(dataUsers);
+  
+  
   return (
-    <Layout pageTitle="Users">
-        <h2>User Page</h2>
+    <Layout pageTitle="Users Page">
+      {dataUsers.map(user => {
+        return(
+          <>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </>
+        )
+      })}
     </Layout>
   )
+}
+
+
+export async function getStaticProps(){
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const dataUsers = await res.json()
+
+  return{
+    props: {
+      dataUsers,
+    }
+  }
 }

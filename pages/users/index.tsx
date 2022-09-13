5d10611920bba22f44bb27f92e6403fe76c9d870
Your Name <samsulr99@gmail.com>
2022-09-13 16:38:37 +0700

    get-static-props

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -15,6 +15,7 @@ export default function User(props: UserProps) {
       {dataUsers.map(user => {
         return(
           <>
+            <p>{user.id}</p>
             <p>{user.name}</p>
             <p>{user.email}</p>
           </>
